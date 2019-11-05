2a9cab90f3f172f61b452563367753f408516fba
SpiralMoon <mjh1660@gmail.com>
2019-11-05 13:34:24 +0900

    Initial commit

---
+++ src/routes/users.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import {Request, Response, NextFunction} from "express";
+
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req:Request, res:Response, next:NextFunction) {
+  res.send('respond with a resource');
+});
+
+export default router;
