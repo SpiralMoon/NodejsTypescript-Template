fbeaaec96b4ec862e84597cdccfbb72a3d3255bc
SpiralMoon <mjh1660@gmail.com>
2019-11-05 17:38:37 +0900

    Update path for common folders (view, public)

--- src/app.ts
+++ src/app.ts
@@ -11,14 +11,14 @@ import {Request, Response, NextFunction} from "express";
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname + '/..', 'views'));
+app.set('views', path.join(__dirname, '..', 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname + '/..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
